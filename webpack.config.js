6e294854fbe8c2a5d1c52e3d68d9c237cae5fea8
Anastasia <naskalelk@gmail.com>
2021-11-14 22:14:45 +0500

    Add files

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const config = {
+    entry: [
+        './src/app.js'
+        ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                use: [
+                    'html-loader',
+                    'pug-html-loader'
+                ]
+            },
+            {
+                test: /\.(sa|c|sc)ss$/,
+                use: [
+                    "style-loader",
+                    miniCss.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                loader: "file-loader",
+                options: {
+                    outputPath: 'images',
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                loader: "file-loader",
+                options: {
+                    outputPath: 'fonts',
+                }
+            }
+        ],
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new miniCss({
+            filename: './css/[name].css'
+        }),
+        new HtmlWebpackPlugin({
+            filename: './html/main.html',
+            template: 'src/main.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: './html/search-room.html',
+            template: 'src/views/search-room/search-room.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: './html/room-details.html',
+            template: 'src/views/room-details/room-details.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: './html/UI.html',
+            template: 'src/views/UI/UI.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: './html/cards.html',
+            template: 'src/views/cards/cards.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: './html/headers-footers-page.html',
+            template: 'src/views/headers-footers-page/headers-footers-page.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: './html/form-elements.html',
+            template: 'src/views/form-elements/form-elements.pug'
+        })
+
+
+    ]
+};
+module.exports = config;
